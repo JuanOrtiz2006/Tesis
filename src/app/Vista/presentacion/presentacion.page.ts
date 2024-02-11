39cc06750b72133858ae2d43d7759ba07b643d3e
JuanOrtiz2006 <juanpabloortiz2006@gmail.com>
2024-02-11 15:41:22 -0500

    Bluetooth

--- src/app/Vista/presentacion/presentacion.page.ts
+++ src/app/Vista/presentacion/presentacion.page.ts
@@ -1,124 +1,75 @@
 import { Component, OnInit } from '@angular/core';
-//Importar Bluethoot Serial
 import { BluetoothSerial } from '@awesome-cordova-plugins/bluetooth-serial/ngx';
-//Importar las Alertas de Ionic
 import { ToastController } from '@ionic/angular';
+
 @Component({
   selector: 'app-presentacion',
   templateUrl: './presentacion.page.html',
   styleUrls: ['./presentacion.page.scss'],
 })
 export class PresentacionPage implements OnInit {
- 
-  //Variables para leer datos
-  pairedList!: any[]; //Lista de dispositivos
-  listToggle: boolean = false; //Detector de listado
-  pairedDevice: number = 0;
-  dataSend: string = "";//Dato enviado
-  dataRead: string = "";//Dato leido
 
-  //Importacion de bluetooth
+  pairedList: any[] = []; 
+  pairedDevice: any; 
+  dataToSend: string = ""; 
+  dataReceived: string = ""; 
+
   constructor(private bluetoothSerial: BluetoothSerial, private toastCtrl: ToastController) {
-    this.checkBluetooth()
+    this.checkBluetooth();
   }
 
-  //Metodo para verificar la nonexion
-  checkBluetooth(){
-    this.bluetoothSerial.isEnabled().then(success=>{
-      this.showError("Bluetooth activado"); // Si el bluetooth esta activado 
-
-    },error=>{
-      this.showError("Por favor, activar bluetooth ")// Si no
-    })
-  }
-  
-  //Metodo para listado de dispositivos
-  pairedListDevice(){
-    this.bluetoothSerial.list().then(success=>{
-      this.pairedList = success;
-      this.listToggle = false;
-    },error=>{
-      this.showError("Por favor, activar bluetooth ");
-      this.listToggle = true;
-    })
+  ngOnInit() {
   }
 
-  //Metodo para seleccionar dispositivo
-  selectDevice() {
-    let connectedDevice = this.pairedList[this.pairedDevice];
-    if (!connectedDevice.address) {
-      this.showError('Select Paired Device to connect');
-      return;
-    }
-    let address = connectedDevice.address;
-    let name = connectedDevice.name;
-
-    this.connect(address);
-  }
-  //Metodo para la conexion
-  connect(address: any) {
-    // Attempt to connect device with specified address, call app.deviceConnected if success
-    this.bluetoothSerial.connect(address).subscribe(success => {
-      this.deviceConnected();
-      this.showError("Successfully Connected");
-    }, error => {
-      this.showError("Error:Connecting to Device");
+  checkBluetooth() {
+    this.bluetoothSerial.isEnabled().then(() => {
+      this.showAlert("Bluetooth activado");
+      this.pairedListDevice();
+    }).catch(() => {
+      this.showAlert("Por favor, activar Bluetooth");
     });
   }
-  //Metodo para suscribirse a la red bluetooth
-  deviceConnected() {
-    // Subscribe to data receiving as soon as the delimiter is read
-    this.bluetoothSerial.subscribe('\n').subscribe(success => {
-      this.handleData(success);
-      this.showError("Connected Successfullly");
-    }, error => {
-      this.showError(error);
+
+  pairedListDevice() {
+    this.bluetoothSerial.list().then(devices => {
+      this.pairedList = devices;
+    }).catch(() => {
+      this.showAlert("Error al obtener la lista de dispositivos pareados");
     });
   }
-  //Metodo para desconectar el dispositivo
-  deviceDisconnected() {
-    // Unsubscribe from data receiving
-    this.bluetoothSerial.disconnect();
-    this.showError("Device Disconnected");
+
+  selectDevice(device: any) {
+    this.pairedDevice = device;
+    this.connectToDevice();
   }
 
-  handleData(data: any) {
-    this.showError(data);
-  }
-  //Metodo para envio de datos
-  sendData() {
-    this.dataSend+='\n';
-    this.showError(this.dataSend);
-
-    this.bluetoothSerial.write(this.dataSend).then(success => {
-      this.showError(success);
-    }, error => {
-      this.showError(error)
+  connectToDevice() {
+    this.bluetoothSerial.connect(this.pairedDevice.address).subscribe(() => {
+      this.showAlert("Conectado al dispositivo");
+      this.sendDataToESP32();
+    }, () => {
+      this.showAlert("Error al conectar al dispositivo");
     });
   }
-  //Metodo para leer datos
-  readData(){
-    this.bluetoothSerial.read().then((data) =>{
-      this.dataRead += data;
-    })
+
+  sendDataToESP32() {
+    this.bluetoothSerial.write("7\n").then(() => {
+      this.showAlert("Datos enviados correctamente");
+    }).catch(() => {
+      this.showAlert("Error al enviar datos");
+    });
   }
-  async showError(message: any) {
+
+  async showAlert(message: string) {
     const toast = await this.toastCtrl.create({
       message: message,
       duration: 2000
     });
     toast.present();
   }
-  
-  ngOnInit() {
+
+  startBluetoothConnection() {
+    // Iniciar la búsqueda de dispositivos Bluetooth
+    this.pairedListDevice();
   }
 }
-
-interface pairedList{
-  "class": number;
-  "id": string,
-  "address": string,
-  "name": string
-
-}
-
